73feed90a53e56abe2bd27f64bf8820c6fa130f3
petardev101 <workdev1989@gmail.com>
2017-12-08 13:50:19 -0600

    basic family of algorithms setup

--- strategy/README.md
+++ strategy/README.md
@@ -1,2 +1,4 @@
 # Strategy Design Pattern
 
+Define a family of algorithms, encapsulates each one and then makes them interchangeable. Ultimately, by leveraging polymorphism, we're able to build loosely coupled
+applications where we are able swap in and out various components at runtime.
--- strategy/index.php
+++ strategy/index.php
@@ -1,3 +1,47 @@
 <?php
 
 require 'vendor/autoload.php';
+
+//encapsulate and make them interchangeable
+interface Logger 
+{
+    public function log($data);
+}
+//Define a family of algorithms
+class LogToFile implements Logger 
+{
+    public function log($data)
+    {
+        var_dump('Log the data to a file');
+    }
+}
+
+class LogToDatabase implements Logger     
+{
+    public function log($data)
+    {
+        var_dump('Log the data to a database');
+    }
+}
+
+class LogToXWebService implements Logger
+{
+    public function log($data)
+    {
+        var_dump('Log the data to a Sass site'); 
+    }   
+}
+
+class App
+{
+    public function log($data)
+    {
+        $logger = new LogToFile;
+
+        $logger->log($data);
+    }
+}
+
+$app = new App;
+
+$app->log('Some information here');
