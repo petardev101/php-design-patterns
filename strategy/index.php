d5784b1a6686a1531a3b5df35332d2e4ca55b21c
petardev101 <workdev1989@gmail.com>
2017-12-08 14:02:58 -0600

    strategy finished

--- strategy/index.php
+++ strategy/index.php
@@ -2,6 +2,20 @@
 
 require 'vendor/autoload.php';
 
+$data = [
+    "one" => 1, 
+    "two" => 2, 
+    "three" => 3, 
+    "four" => 4, 
+    "five" => 5, 
+    "six" => 6, 
+    "seven" => 7, 
+    "eight" => 8, 
+    "nine" => 9
+];
+
+print_r($data);
+
 //encapsulate and make them interchangeable
 interface Logger 
 {
@@ -34,9 +48,9 @@ class LogToXWebService implements Logger
 
 class App
 {
-    public function log($data)
+    public function log($data, Logger $logger = null)
     {
-        $logger = new LogToFile;
+        $logger = $logger ?: new LogToFile;
 
         $logger->log($data);
     }
@@ -44,4 +58,4 @@ class App
 
 $app = new App;
 
-$app->log('Some information here');
+$app->log('Some information here', new LogToDatabase);
