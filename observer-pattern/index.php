4a3f381fdffcc7ad101bb15640e9619d7c7f9e49
petardev101 <workdev1989@gmail.com>
2017-12-15 08:15:22 -0600

    observer done

--- observer-pattern/index.php
+++ observer-pattern/index.php
@@ -2,9 +2,9 @@
 
 interface Subject
 {
-    public function attach($observer); //attach an observer
+    public function attach($observable); //attach an observer
     public function detach($observer); //detach an observer
-    public function release($observer); //release method that cleans out array of observers and returns it to client
+    public function notify(); //release method that cleans out array of observers and returns it to client
 } //like the "Publisher"
 
 interface Observer 
@@ -12,22 +12,77 @@ interface Observer
     public function handle(); //triggers observer
 } //like the "Subscriber"
 
-class Auth implements Subject       
+class Login implements Subject       
 {
     protected $observers = [];
-
-    public function attach($observer)
+    
+    public function attach($observable)
     {
-        $this->observers[] = $observer;
+        if (is_array($observable)) {
+            return $this->attachObservers($observable);
+        }   
+        $this->observers[] = $observable;
+
+        return $this;
     }
 
-    public function detach($observer)
+    public function detach($index)
     {
-        
+        unset($this->observers[$index]);
     }
 
-    public function release($observer)
+    public function notify($observer)
     {
-        
+         foreach ($this->observers as $observer) {
+            $observer->handle();
+         }
+    }
+
+    private function attachObservers($observable)
+    {
+        foreach ($observable as $observer) {
+            if (! $observer instanceof Observer) {
+                throw new Exception;
+            }
+
+            $this->attach($observer);
+        }
+    }
+
+    public function fire()
+    {
+        $this->login;
     }
 }
+
+class LogHandler implements Observer
+{
+    public function handle()
+    {
+        var_dump('log something important');
+    }
+}
+
+class EmailNotifier implements Observer
+{
+    public function handle()
+    {
+        var_dump('fire off an email.');
+    }
+}
+
+class LoginReporter implements Observer
+{
+    public function handle()
+    {
+        var_dump('do some reporting.');
+    }
+}
+
+$login = new login;
+$login->attach([
+    new LogHandler,
+    new EmailNotifier,
+    new LoginReporter
+]);
+$login->fire();
