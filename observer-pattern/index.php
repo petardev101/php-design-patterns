9ba49510472909c8b8a5e63312f2bc9727d01b93
petardev101 <workdev1989@gmail.com>
2017-12-15 07:36:36 -0600

    working on observer pattern

--- observer-pattern/index.php
+++ observer-pattern/index.php
@@ -1,13 +1,33 @@
 <?php
 
-interface Subject 
+interface Subject
 {
-    public function attach($observer);
-    public function detach($observer);
-    public function release($observer);
-}
+    public function attach($observer); //attach an observer
+    public function detach($observer); //detach an observer
+    public function release($observer); //release method that cleans out array of observers and returns it to client
+} //like the "Publisher"
 
 interface Observer 
 {
-    public function handle();
+    public function handle(); //triggers observer
+} //like the "Subscriber"
+
+class Auth implements Subject       
+{
+    protected $observers = [];
+
+    public function attach($observer)
+    {
+        $this->observers[] = $observer;
+    }
+
+    public function detach($observer)
+    {
+        
+    }
+
+    public function release($observer)
+    {
+        
+    }
 }
